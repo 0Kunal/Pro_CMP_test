1ce5c53c70cf15ffac4ba1f13ace8e70348a1685
Kunal Kamat <kunalkamat3@gmail.com>
2025-05-16 11:23:45 +0530

    additional test

--- tests/consent-banner.spec.js
+++ tests/consent-banner.spec.js
@@ -11,27 +11,27 @@ test.describe("Consent Banner Behavior", () => {
     await expect(banner).toBeVisible();
   });
 
-  // test("should accept consent and hide the banner", async ({ page }) => {
-  //   const acceptBtn = page.locator('[id="gravitoLightCMP-layer1-accept-all"]');
-  //   await expect(acceptBtn).toBeVisible();
-  //   await acceptBtn.click();
-  //   await expect(acceptBtn).not.toBeVisible();
-  // });
+  test("should accept consent and hide the banner", async ({ page }) => {
+    const acceptBtn = page.locator('[id="gravitoTCFCMP-layer1-accept-all"]');
+    await expect(acceptBtn).toBeVisible();
+    await acceptBtn.click();
+    await expect(acceptBtn).not.toBeVisible();
+  });
 
-  // test("should chnage the tab when clicking on the 'Details' button", async ({
-  //   page,
-  // }) => {
-  //   const detailsBtn = page.locator(
-  //     '[class="gravitoPROCMP-tabs-div"] > button:nth-of-type(2)'
-  //   );
-  //   await expect(detailsBtn).toBeVisible();
-  //   await detailsBtn.click();
+  test("should change the tab when clicking on the 'Details' button", async ({
+    page,
+  }) => {
+    const detailsBtn = page.locator(
+      '[class="gravitoPROCMP-tabs-div"] > button:nth-of-type(2)'
+    );
+    await expect(detailsBtn).toBeVisible();
+    await detailsBtn.click();
 
-  //   const detailsTab = page.locator(
-  //     '[class="gravitoPROCMP-GCMP-layer2-container"]'
-  //   );
-  //   await expect(detailsTab).toBeVisible();
-  // });
+    const detailsTab = page.locator(
+      '[class="gravitoPROCMP-GCMP-layer1-container"]'
+    );
+    await expect(detailsTab).toBeHidden();
+  });
 
   // test("should check the number of purposes in the details tab is equal to 5", async ({
   //   page,
